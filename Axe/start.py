d26692e21b6021392997bf4dd00aeae309907aaf
Monk-xD <108565532+Monk-xD@users.noreply.github.com>
2023-06-15 14:24:33 +0530

    Update start.py

--- Axe/start.py
+++ Axe/start.py
@@ -1,6 +1,7 @@
+from Axe import *
 from pyrogram import Client, filters 
 from pyrogram.types import *
 
-@Client.on_message(filters.private & filters.command("start"))
+@AxeDL.on_message(filters.private & filters.command("start"))
 async def start(client: Client, message: Message):
         await message.reply("Just send me an artist and/or a song name and i will download music for you free!")  
